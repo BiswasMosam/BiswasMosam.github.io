5d2f83cdc35f6e5c4d330d3d647ef17a2c46e366
BiswasMosam <mosambiswas999@gmail.com>
2026-09-17 02:04:16 +0530

    Add hidden cursor bubble messages on copyright footers, bump SW to v16

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@
 
    Bump VERSION on every deploy. The name change is what evicts the old cache. */
 
-const VERSION = 'v15';
+const VERSION = 'v16';
 const CACHE = `mosam-biswas-portfolio-${VERSION}`;
 
 /* Everything needed to render all four pages with the network switched off.
